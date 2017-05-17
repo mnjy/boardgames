4d7e077f9e49fef0b3c4c8e2f6da9bffea181b78
Misha <mnjy@mit.edu>
2017-05-16 21:39:18 -0400

    First commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public Vector3 rotator = new Vector3(0,0,0);
+    
+	void Update () {
+        transform.Rotate(rotator);
+	}
+}
